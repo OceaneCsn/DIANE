55a4d84fa743f4a04b0adc305509c65862bc0ac2
Oceane Cassan <oceane.cssn@gmail.com>
2020-05-21 23:01:16 +0200

    ui start

--- R/mod_network_inference.R
+++ R/mod_network_inference.R
@@ -10,7 +10,63 @@
 mod_network_inference_ui <- function(id){
   ns <- NS(id)
   tagList(
-    shiny::h1("Network inference")
+    shiny::h1("Network inference"),
+    
+    col_3(
+      boxPlus(
+        title = "Settings",
+        solidHeader = FALSE,
+        status = "success",
+        collapsible = TRUE,
+        closable = FALSE,
+        width = 12,
+        
+        col_10(shiny::h4("GENIE3 Gene regulatory network inferenceinsta")),
+        
+        col_2(shinyWidgets::dropdownButton(
+          size = 'xs',
+          shiny::includeMarkdown(system.file("extdata", "normalisation.md", package = "DIANE")),
+          circle = TRUE,
+          status = "success",
+          icon = shiny::icon("question"),
+          width = "600px",
+          tooltip = shinyWidgets::tooltipOptions(title = "More details")
+        )),
+        
+        
+        shiny::br(),
+        shiny::hr(),
+        shiny::fluidRow(col_10(shiny::uiOutput(ns("input_genes")))),
+        shiny::hr(),
+        shiny::fluidRow(col_12(shiny::uiOutput(ns("input_conditions_choice")))),
+        
+        shiny::hr(),  
+        shiny::fluidRow(col_12(shinyWidgets::sliderTextInput(
+          inputId = ns("cluster_range"),
+          label = "Min mand max number of clusters to test :", 
+          choices = seq(3,15),
+          selected = c(6, 9),
+          from_min = 3, 
+          from_max = 7,
+          to_min = 8,
+          to_max = 15
+        ))),
+        shiny::br(),
+        
+        shiny::fluidRow(
+          col_12(shinyWidgets::actionBttn(
+            ns("launch_coseq_btn"),
+            label = "Launch clustering",
+            color = "success",
+            style = 'bordered'
+          ))),
+        
+        
+        shiny::hr(),
+        shiny::uiOutput(ns("coseq_summary"))
+        
+      )
+    )
  
   )
 }
