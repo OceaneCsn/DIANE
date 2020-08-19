008bfb134b50fa1bcdc0237049237952dd9d5ef0
Oceane Cassan <oceane.cassan@supagro.fr>
2020-08-19 16:11:13 +0200

    forced reactive arguments before parallel

--- R/run_app.R
+++ R/run_app.R
@@ -6,7 +6,7 @@
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
-run_app <- function(
+run_app <- function(server_version = FALSE,
   ...
 ) {
   golem::with_golem_options(
@@ -15,6 +15,6 @@ run_app <- function(
       server = app_server,
       options = list(host = "0.0.0.0")
     ),
-    golem_opts = list(server_version = FALSE, ...)
+    golem_opts = list("server_version" = server_version , ...)
   )
 }
