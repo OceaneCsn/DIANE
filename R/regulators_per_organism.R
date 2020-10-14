453a3ffb2c85080ecc3d4fdb70a281b9a337db17
Oceane Cassan <oceane.cssn@gmail.com>
2020-10-14 22:00:41 +0200

    added e coli tfs list

--- R/regulators_per_organism.R
+++ R/regulators_per_organism.R
@@ -10,6 +10,7 @@
 #'  \item{Mus musculus}{\url{http://bioinfo.life.hust.edu.cn/AnimalTFDB/#!/download}}
 #'  \item{Drosophilia melanogaster}{\url{https://flybase.org/reports/FBgg0000745.html}}
 #'  \item{C elegans}{\url{http://www.wormbook.org/chapters/www_transcriptionalregulation.2/transregulate.html#sec5-1}}
+#'  \item{E coli}{\url{http://regulondb.ccg.unam.mx/menu/download/datasets/index.jsp}}
 #'  }
 #' @format A named list with the following elements:
 #' \describe{
@@ -18,11 +19,15 @@
 #'  \item{Mus musculus}{Character vector of the known transcription factors in Mouse}
 #'  \item{Drosophilia melanogaster}{Character vector of the known transcription factors in Drosophilia melanogaster}
 #'  \item{Caenorhabditis elegans}{Character vector of the known transcription factors in C elegans}
+#'  \item{Escherichia coli}{Character vector of the known transcription factors and sigma elements in E coli K12}
 #' }
 #' @examples
 #' {
 #'  print(head(regulators_per_organism[["Arabidopsis thaliana"]]))
 #'  print(head(regulators_per_organism[["Homo sapiens"]]))
 #'  print(head(regulators_per_organism[["Mus musculus"]]))
+#'  print(head(regulators_per_organism[["Drosophilia melanogaster"]]))
+#'  print(head(regulators_per_organism[["Caenorhabditis elegans"]]))
+#'  print(head(regulators_per_organism[["Escherichia coli"]]))
 #' }
 "regulators_per_organism"
