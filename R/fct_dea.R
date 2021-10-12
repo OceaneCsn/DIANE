464ba8045e6b860f6cbd2e03eed69c3b8df931f2
Alexandre-So <alex.soriano@laposte.net>
2021-10-12 12:12:23 +0200

    Modify DEA function to allow multiple input conds
    The old function has been saved as "legacy".

--- R/fct_dea.R
+++ R/fct_dea.R
@@ -59,7 +59,7 @@ estimateDispersion <- function(tcc, conditions = NULL) {
   return(fit)
 }
 
-#' Estimates Differentially Expression Genes
+#' Estimates Differentially Expression Genes. Legacy version.
 #'
 #' @description Given a glmFit model, performs log ratio tests using edgeR 
 #' function glmLRT and
@@ -91,7 +91,7 @@ estimateDispersion <- function(tcc, conditions = NULL) {
 #' topTags <- DIANE::estimateDEGs(fit, reference = "C", perturbation = "H", p.value = 0.01)
 #' DEGs <- topTags$table
 #' head(DEGs)
-estimateDEGs <- function(fit, reference, perturbation, p.value = 1, lfc = 0) {
+estimateDEGs_legacy <- function(fit, reference, perturbation, p.value = 1, lfc = 0) {
   contrast <-
     ifelse(colnames(fit$design) == reference,
            -1,
@@ -102,6 +102,87 @@ estimateDEGs <- function(fit, reference, perturbation, p.value = 1, lfc = 0) {
   return(top)
 }
 
+#' Estimates Differentially Expression Genes
+#'
+#' @description Given a glmFit model, performs log ratio tests using edgeR 
+#' function glmLRT and
+#' returns the genes that are differentially expressed
+#' between the reference and perturbation conditions.
+#' log ratios are expressed as log(perturbation/reference)
+#'
+#' @param fit edgeR glmFit
+#' @param reference condition (or a vector of conditions) being considered as the reference for differential analysis.
+#' It should corresponds to a condition name, e.g. the string before the underscore and 
+#' replicate number in your sample names.
+#' @param perturbation condition (or a vector of conditions) we compared to the reference for differential analysis.
+#' It should corresponds to a condition name, e.g. the string before the underscore and 
+#' replicate number in your sample names.
+#' @param p.value numeric cutoff value for adjusted p-values. Only tags with adjusted p-values equal or 
+#' lower than specified are returned
+#' @param lfc minimal absolute log fold change required for a gene to be considered as 
+#' differentially expressed.
+#' @param systematic_orientation If multiple conditions are indicated in reference or perturbation, compute all
+#' comparisons and return only genes that have the same orientation in all comparisons. If False (default), the mean
+#' of conditions is used.
+#' @return topTags object, which table element contains DEGs dataframe.
+#' @export
+#' @examples
+#' data("abiotic_stresses")
+#' tcc_object <- DIANE::normalize(abiotic_stresses$raw_counts, 
+#' abiotic_stresses$conditions, iteration = FALSE)
+#' threshold = 10*length(abiotic_stresses$conditions)
+#' tcc_object <- DIANE::filter_low_counts(tcc_object, threshold)
+#' fit <- DIANE::estimateDispersion(tcc = tcc_object, 
+#' conditions = abiotic_stresses$conditions)
+#' topTags <- DIANE::estimateDEGs(fit, reference = "C", perturbation = "H", p.value = 0.01)
+#' DEGs <- topTags$table
+#' head(DEGs)
+estimateDEGs <- function(fit, reference, perturbation, p.value = 1, lfc = 0, systematic_orientation = FALSE) {
+  contrast <-  create_versus_design(colnames(fit$design), reference, perturbation)
+  print(contrast)
+  lrt <- edgeR::glmLRT(fit, contrast = contrast)
+  top <- edgeR::topTags(lrt, p.value = p.value, n = Inf)
+  top$table <- top$table[abs(top$table$logFC) > lfc,]
+  
+  ###Complicated case. 
+  if (systematic_orientation == TRUE &&
+      length(reference) + length(perturbation) > 2) {
+    all_vs_all_deg <- list()
+    for (ref in colnames(fit$design)[contrast > 0]) {
+      ###Reference
+      for (pert in colnames(fit$design)[contrast < 0]) {
+        ###vs perturbation
+        print(paste0(ref," vs ",pert))
+        targeted_contrast <-
+          ifelse(colnames(fit$design) == ref,
+                 -1,
+                 ifelse(colnames(fit$design) == pert, 1, 0))
+        targeted_lrt <-
+          edgeR::glmLRT(fit, contrast = targeted_contrast)
+        targeted_top <-
+          edgeR::topTags(targeted_lrt, p.value = p.value, n = Inf) ###TODO : change pvalue cutoff for lest restictive one.
+        all_vs_all_deg[[paste0(ref, "_vs_", pert)]] <-
+          targeted_top$table[abs(targeted_top$table$logFC) > lfc, ]
+      }
+    }
+    
+    ###Remove genes that are not DE in each condition.
+    DE_genes <- top$table$genes
+    upreg = DE_genes
+    downreg = DE_genes
+    for (comp in names(all_vs_all_deg)) {
+      print(comp)
+      upreg <-
+        upreg[upreg %in% rownames(all_vs_all_deg[[comp]][which(all_vs_all_deg[[comp]][2] >= 1), ])]
+      downreg  <-
+        downreg[downreg %in% rownames(all_vs_all_deg[[comp]][which(all_vs_all_deg[[comp]][2] <= -1), ])]
+    }
+    top$table <- top$table[c(upreg, downreg), ]
+    
+  }
+  
+  return(top)
+}
 
 #' MA or volcano plot for differential expression results
 #'
@@ -172,4 +253,49 @@ draw_venn <- function(gene_list){
     stop("The number of gene lists must be between 2 and 4 to be shown in Venn diagram")
   venn <- ggVennDiagram::ggVennDiagram(gene_list, color = "#888888")
   suppressMessages(venn + ggplot2::scale_fill_gradient(low = "#EEEEEE", high = "#61BF45"))
+}
+
+#' create_versus_design
+#' 
+#' @description Given a vector containing an experiment design and two other vectors
+#' containg reference and comparison condition, return a design vector for 
+#' equilibrate comparison in edgeR
+#'
+#'
+#' @param experiment_design A vector containing all the conditions of a studied dataset.
+#' @param reference_point All the points in experiment_design to consider as reference
+#' @param comparison_point All the points in experiment_design to consider as comparison points.
+#'
+#' @noRd
+#' @return
+#'
+#' @examples create_versus_design(c("reference","condition1","condition2","condition3"), c("condition1", "condition2", "condition3"), "reference")
+create_versus_design <- function(experiment_design, reference_point, comparison_point){
+  experiment_design = unique(experiment_design) ###We just need the name of the different expriment design. We dont care about the replicates or so.
+  reference_point = unique(reference_point)
+  comparison_point = unique(comparison_point)
+  
+  ###Check that all the specified points are in the experiment design
+  if (!all(c(reference_point, comparison_point) %in% experiment_design)) {
+    stop(
+      paste0(
+        c(reference_point, comparison_point)[!c(reference_point, comparison_point) %in% experiment_design],
+        "not in design (experiment_design). Please check your input condition list."
+      )
+    )
+  }
+  ###Check that there is no common reference and perturbation.
+  if (any(reference_point %in% comparison_point) | any(comparison_point %in% reference_point)){
+    stop("A condition cannot be used as reference and comparison point. Please check your comparison vector.")
+  }
+  
+  comparison_matrix=rep(0, length(experiment_design))
+  
+  weight_reference=1/length(reference_point) ###We want to have an equel weight for all the points. It means that it must sum up to 1.
+  weight_comparison=1/length(comparison_point)
+  
+  comparison_matrix[which(experiment_design %in% reference_point)] <- -weight_reference
+  comparison_matrix[which(experiment_design %in% comparison_point)] <- weight_comparison
+  
+  return(comparison_matrix)
 }
